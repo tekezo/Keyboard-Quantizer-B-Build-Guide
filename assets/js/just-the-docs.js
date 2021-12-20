1abff3cdc026412d4fd10fc4606d7e44f65810e9
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2021-12-20 23:13:31 +0900

    Add v0.11.1

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -53,7 +53,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'https://sekigon-gonnoc.github.io/Keyboard-Quantizer-B-Build-Guide/assets/js/search-data.json', true);
+  request.open('GET', 'http://localhost:4000/Keyboard-Quantizer-B-Build-Guide/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
@@ -430,7 +430,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', 'https://sekigon-gonnoc.github.io/Keyboard-Quantizer-B-Build-Guide/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', 'http://localhost:4000/Keyboard-Quantizer-B-Build-Guide/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Document ready

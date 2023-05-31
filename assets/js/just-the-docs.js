75b7f270c6367f02403c2e03ccaf626e6fc3535b
Takayama Fumihiko <tekezo@pqrs.org>
2023-06-01 08:25:02 +0900

    Apply prettier

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -59,11 +59,7 @@
 
   function initSearch() {
     var request = new XMLHttpRequest();
-    request.open(
-      "GET",
-      "/assets/js/search-data.json",
-      true
-    );
+    request.open("GET", "/assets/js/search-data.json", true);
 
     request.onload = function () {
       if (request.status >= 200 && request.status < 400) {
@@ -491,12 +487,7 @@
 
   jtd.setTheme = function (theme) {
     var cssFile = document.querySelector('[rel="stylesheet"]');
-    cssFile.setAttribute(
-      "href",
-      "/assets/css/just-the-docs-" +
-        theme +
-        ".css"
-    );
+    cssFile.setAttribute("href", "/assets/css/just-the-docs-" + theme + ".css");
   };
 
   // Document ready
